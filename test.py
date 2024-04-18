eb83ac42fa33d888171e6d2fcb1fe8127a6fcee5
David Zambrano <82787004+econdavidzh@users.noreply.github.com>
2024-04-18 18:51:38 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.express as px
+#import pandas as pd
+#import numpy as np
+#import plotly.express as px
 
 
 # Configuración de la App:
